e4cef59830654867862db09fcd94f7101bdcc3f1
Skobert <dogbert756@gmail.com>
2024-08-30 19:39:14 -0500

    minor changes - adding lint rules and doc updates

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 # CrysisRecruits
 
-## Logical Architecture
+## Architecture
+
+### Logical Architecture
 
 ![](doc/img/crysis%20recruitbot%20arch-Logical.jpg)
 
-## Physical Infrastructure Architecture
+### Physical Infrastructure Architecture
 
 ![](doc/img/crysis%20recruitbot%20arch-SVC%20Physical.jpg)
 
-## Cron Service Components
+### Cron Service Components
 
 ![](doc/img/crysis%20recruitbot%20arch-SVC%20Component.jpg)
 
-## Discord Bot Components
+### Discord Bot Components
 ![](doc/img/crysis%20recruitbot%20arch-Bot%20Component.jpg)
+
+
+## Lambda Config
+
+To set up environment variables locally, add a .env file to the root folder. .gitignore will take care of ignoring it.\
+Format of the .env file is always \
+`KEY1=value1`\
+`KEY2=value2`\
+Each key separated by newline.
+
+**Environment Variables:**
+- SCRAPENINJA_API_KEY
+
+
+## Bot Config
+
+**Environment Variables:**
+- None
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,55 @@
-import globals from "globals"
+import globals from 'globals'
 
 export default [
-  {
-    languageOptions: { globals: globals.browser },
-    rules: {
-      "no-console": "warn",
+    {
+        languageOptions: { globals: globals.browser },
+        rules: {
+            '1no-console': 'warn',
 
-      "no-undef": "error",
-      "semi": [2, "never"],
-      "semi-spacing": "error",
-      //
-      "eqeqeq": "warn",
-      "no-unexpected-multiline": 2,
-      "no-invalid-this": "error",
-      "no-return-assign": "error",
-      "no-unused-expressions": ["error", { "allowTernary": true }],
-      "no-useless-concat": "error",
-      "no-useless-return": "error",
-      "no-constant-condition": "warn",
-      "no-unused-vars": ["warn", { "argsIgnorePattern": "req|res|next|__" }],
-      //* Enhance Readability
-      "indent": ["error", 4],
-      "no-mixed-spaces-and-tabs": "warn",
-      "space-before-blocks": "error",
-      "space-in-parens": "error",
-      "space-infix-ops": "error",
-      "space-unary-ops": "error",
-      "quotes": ["error", "single"],
-      //
-      "max-len": ["error", { "code": 200 }],
-      "max-lines": ["error", { "max": 500 }],
-      "keyword-spacing": "error",
-      "multiline-ternary": ["error", "never"],
-      "no-mixed-operators": "error",
-      //
-      "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1 }],
-      "no-whitespace-before-property": "error",
-      "nonblock-statement-body-position": "error",
-      "object-property-newline": [
-        "error",
-        { "allowAllPropertiesOnSameLine": true }
-      ],
-      //* ES6
-      "arrow-spacing": "error",
-      "no-confusing-arrow": "error",
-      "no-duplicate-imports": "error",
-      "no-var": "error",
-      "object-shorthand": "off",
-      "prefer-const": "error",
-      "prefer-template": "warn"
+            'no-undef': 'error',
+            'semi': [2, 'never'],
+            'semi-spacing': 'error',
+            //
+            'eqeqeq': 'warn',
+            'no-unexpected-multiline': 2,
+            'no-invalid-this': 'error',
+            'no-return-assign': 'error',
+            'no-unused-expressions': ['error', { 'allowTernary': true }],
+            'no-useless-concat': 'error',
+            'no-useless-return': 'error',
+            'no-constant-condition': 'warn',
+            'no-unused-vars': ['warn', { 'argsIgnorePattern': 'req|res|next|__' }],
+            //* Enhance Readability
+            'indent': ['error', 4],
+            'no-mixed-spaces-and-tabs': 'warn',
+            'space-before-blocks': 'error',
+            'space-in-parens': 'error',
+            'space-infix-ops': 'error',
+            'space-unary-ops': 'error',
+            'quotes': ['error', 'single'],
+            //
+            'max-len': ['error', { 'code': 200 }],
+            'max-lines': ['error', { 'max': 500 }],
+            'keyword-spacing': 'error',
+            'multiline-ternary': ['error', 'never'],
+            'no-mixed-operators': 'error',
+            //
+            'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 1 }],
+            'no-whitespace-before-property': 'error',
+            'nonblock-statement-body-position': 'error',
+            'object-property-newline': [
+                'error',
+                { 'allowAllPropertiesOnSameLine': true }
+            ],
+            //* ES6
+            'arrow-spacing': 'error',
+            'no-confusing-arrow': 'error',
+            'no-duplicate-imports': 'error',
+            'no-var': 'error',
+            'object-shorthand': 'off',
+            'prefer-const': 'error',
+            'prefer-template': 'warn'
+        },
+        files: ['**/*.js', '**/*.mjs', '**/*.cjs']
     },
-    files: ["**/*.js", "**/*.mjs", "**/*.cjs"]
-  },
 ]
